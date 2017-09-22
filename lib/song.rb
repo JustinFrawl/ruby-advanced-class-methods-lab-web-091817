2af0c2bd1f25e44b7050bdb9c81e76824531a478
github username <github email address>
2017-09-22 15:59:11 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +11,53 @@ class Song
   def save
     self.class.all << self
   end
+  def self.destroy_all
+    @@all.clear
+  end
+  def self.create
+    Song.new.save
+    self.all.last
+  end
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+  def self.create_by_name(name)
+    song = Song.create
+    song.name = name
+    song
+  end
+  def self.find_by_name(name)
+    @@all.detect{|song_name| song_name.name == name}
+  end
+  def self.find_or_create_by_name(name)
+    if !find_by_name(name)
+      create_by_name(name)
+    else
+      find_by_name(name)
 
+    end
+  end
+  def self.alphabetical
+    self.all.sort_by{|s| s.name}
+  end
+  def self.new_from_filename(file_name)
+    parts = file_name.split(" - ")
+    artist_name = parts[0]
+    name = parts[1].gsub(".mp3","")
+    song = self.new
+    song.name = name
+    song.artist_name = artist_name
+    song
+end
+def self.create_from_filename(file_name)
+  parts = file_name.split(" - ")
+  artist_name = parts[0]
+  name = parts[1].gsub(".mp3","")
+  song = self.create
+  song.name = name
+  song.artist_name = artist_name
+  song
+end
 end
